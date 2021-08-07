f80ae787bca87312651bf75801fa01ad6c39d611
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-07 11:07:29 -0500

    added majority of item tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,25 @@
 describe("Gilded Rose", function() {
+  describe("Majority of Items", function() {
+    it("should degrade quality of item twice as fast, after sellIn date passed", function() {
+      items = [new Item("+5 Dexterity Vest", 0, 4)];
+      update_quality();
+      expect(items[0].sell_in).toBe(-1);
+      expect(items[0].quality).toBe(2);
+    });
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
+    it("Quality of item is never negative", function() {
+      items = [new Item("+5 Dexterity Vest", 10, 0)];
+      update_quality();
+      expect(items[0].sell_in).toBe(9);
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("Quality of item is never more than 50", function() {
+      items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)];
+      update_quality();
+      expect(items[0].sell_in).toBe(4);
+      expect(items[0].quality).toBe(50);
+    });
+});
 
 });
